cb438fb54de6ebfb21c03330fc604da77c7ef7f4
Ryan <naamloosdt@gmail.com>
2020-12-16 00:47:11 +0100

    base Chip8 class, Registers, Screen/Keyboard/Sound interfaces

---
+++ Crisp8/Crisp8/Chip8.cs
@@ -0,0 +1,34 @@
+﻿// https://github.com/mattmikolay/chip-8/wiki/CHIP%E2%80%908-Technical-Reference#references
+// https://github.com/mattmikolay/chip-8/wiki/CHIP%E2%80%908-Instruction-Set
+// http://devernay.free.fr/hacks/chip8/C8TECH10.HTM
+using Crisp8.Interfaces;
+using System;
+
+namespace Crisp8
+{
+    public class Chip8
+    {
+        private int current = 0;
+        private byte[] ram = new byte[4096];
+        private Registers registers = new Registers();
+
+        private IScreen screen;
+        private ISoundHandler sound;
+        private IKeyboard keyboard;
+
+        public Chip8(byte[] rom, IScreen screen, ISoundHandler sound, IKeyboard keyboard)
+        {
+            this.ram = rom;
+
+            this.screen = screen;
+            this.sound = sound;
+            this.keyboard = keyboard;
+        }
+
+        private void runInstruction(short instruction)
+        {
+            // an instruction is 1 short, so 2 bytes, so 4 nibbles. C# has no nibble type so we'll have to figure that out ourselves.
+            // bitmasking time! :D ( D: )
+        }
+    }
+}
---
+++ Crisp8/Crisp8/Registers.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crisp8
+{
+    public struct Registers
+    {
+        /// <summary>
+        /// Register 0
+        /// </summary>
+        public byte V0;
+
+        /// <summary>
+        /// Register 1
+        /// </summary>
+        public byte V1;
+
+        /// <summary>
+        /// Register 2
+        /// </summary>
+        public byte V2;
+
+        /// <summary>
+        /// Register 3
+        /// </summary>
+        public byte V3;
+
+        /// <summary>
+        /// Register 4
+        /// </summary>
+        public byte V4;
+
+        /// <summary>
+        /// Register 5
+        /// </summary>
+        public byte V5;
+
+        /// <summary>
+        /// Register 6
+        /// </summary>
+        public byte V6;
+
+        /// <summary>
+        /// Register 7
+        /// </summary>
+        public byte V7;
+
+        /// <summary>
+        /// Register 8
+        /// </summary>
+        public byte V8;
+
+        /// <summary>
+        /// Register 9
+        /// </summary>
+        public byte V9;
+
+        /// <summary>
+        /// Register A
+        /// </summary>
+        public byte VA;
+
+        /// <summary>
+        /// Register B
+        /// </summary>
+        public byte VB;
+
+        /// <summary>
+        /// Register C
+        /// </summary>
+        public byte VC;
+
+        /// <summary>
+        /// Register D
+        /// </summary>
+        public byte VD;
+
+        /// <summary>
+        /// Register E
+        /// </summary>
+        public byte VE;
+
+        /// <summary>
+        /// Register F, also often used as flags.
+        /// </summary>
+        public byte VF;
+
+
+        /// <summary>
+        /// Register I, generally used to store memory adresses. Only the lowest (rightmost) 12 bits are used
+        /// </summary>
+        public short I;
+
+
+        /// <summary>
+        /// Register PC (program counter), used by the CPU to store the current executing address.
+        /// </summary>
+        public short PC;
+
+        /// <summary>
+        /// Register SP (Stack pointer), used by the CPU to point to the topmost level of the stack.
+        /// </summary>
+        public byte SP;
+    }
+}
